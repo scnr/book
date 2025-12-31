fd524870bbcfb719269e4e549ea549d5494e0886
zapotek <@3$%1pr0t0S3CW3b4P&8)(>
2025-12-31 18:46:25 +0200

    Remove heroku related code

--- Gemfile
+++
@@ -1,1 +0,0 @@
-gem 'rack', github: 'rack/rack'
--- app.rb
+++
@@ -1,7 +0,0 @@
-require 'webrick'
-
-server = WEBrick::HTTPServer.new(Port: 80)
-server.mount('/', WEBrick::HTTPServlet::FileHandler, 'book/')
-
-trap("INT") { server.stop } # Stop the server with Ctrl+C
-server.start
